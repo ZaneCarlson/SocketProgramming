d0502ba9549f2dfe1b7d46940b3b3fe950efd9ea
ZaneCarlson <146386094+ZaneCarlson@users.noreply.github.com>
2025-04-05 01:14:39 -0400

    Added client commands (NEED COMMENTS)

--- client.py
+++ client.py
@@ -1,42 +1,59 @@
 import socket
+import sys
 
-HOST = '127.0.0.1'
+if len(sys.argv) != 2:
+    print("Usage: python client.py <server_ip_address>")
+    sys.exit(1)
+
+serverIP = sys.argv[1]
 SERVER_PORT = 6789
 
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_client.connect((HOST, SERVER_PORT))
+socket_client.connect((serverIP, SERVER_PORT))
 
 while True:
 
-    request = input("Enter your request (MSGGET, MSGSTORE, QUIT, or SHUTDOWN): ")
-    if request == "QUIT":
-        socket_client.sendall(request.encode())
-        while True:
-            msg = socket_client.recv(1024).decode()
-            if msg == "200 OK":
-                print(msg)
-                break
-        break
-    
+    request = input("Enter your request (MSGGET, MSGSTORE, QUIT, or SHUTDOWN): \nc: ")
     request += "\n"
 
-    #if request == "SHUTDOWN\n":
-        
-    #    print("Server is shutting down.")
-    #    socket_client.sendall(request.encode())
-    #    break
-
-    if request == "MSGGET\n":
+    if request == "MSGSTORE\n":
+            socket_client.sendall(request.encode())
+            recieved_msg = socket_client.recv(1024).decode()
+            print("s:" + recieved_msg)
+            sending_msg = input("c:")
+            socket_client.sendall(sending_msg.encode())
+            recieved_msg = socket_client.recv(1024).decode()
+            print("s:" + recieved_msg)
+            continue
+    
+    elif request == "QUIT\n":
         socket_client.sendall(request.encode())
-        while True:
-            msg = socket_client.recv(1024).decode()
-            if msg == "200 OK":
-                print(msg)
-                break
+        msg = socket_client.recv(1024).decode()
+        if msg == "200 OK":
+            print("s:" + msg)
+        break
 
-    #elif request == "MSGSTORE\n":
-    #    msg = input("enter your message to store:")
-    #    socket_client.sendall(msg.encode())
+    elif request == "SHUTDOWN\n":
+       socket_client.sendall(request.encode())
+       recieved_msg = socket_client.recv(1024).decode()
+       print("s:" + recieved_msg)
+       sending_msg = input("c:")
+       socket_client.sendall(sending_msg.encode())
+       recieved_msg = socket_client.recv(1024).decode()
+       if recieved_msg == "200 OK SHUTTING DOWN":
+            break
+       elif recieved_msg == "400 INVALID PASSWORD":
+            print("s:" + recieved_msg)
+            continue
+
+    elif request == "MSGGET\n":
+        socket_client.sendall(request.encode())
+        recieved_msg = socket_client.recv(1024).decode()
+        print("s:" + recieved_msg)
+        continue
+
+    
+
 
     else:
         print("Invalid request. Please try again.")
--- server.py
+++ server.py
@@ -13,11 +13,47 @@ print(f"Server is listening on {HOST}:{SERVER_PORT}")
 while True:
     socket_client, client_address = socket_server.accept()
     print(f"Connection from {client_address} has been established.")
-    socket_client.sendall(message.encode())
 
-    data = socket_client.recv(1024).decode()
-    if data == "MSGGET\n":
-        msg = "200 OK\n" + message
-        socket_client.sendall(msg.encode())
 
-    socket_client.close()
+    while True:
+        data = socket_client.recv(1024).decode()
+        print(f"Received request: {data.strip()}")
+        if data == "MSGGET\n":
+            msg = "200 OK\n" + message
+            socket_client.sendall(msg.encode())
+            continue
+        elif data == "MSGSTORE\n":
+            msg = "200 OK"
+            socket_client.sendall(msg.encode())
+            data = socket_client.recv(1024).decode()
+            print(f"Received message: {data.strip()}")
+            message = data.strip()
+            msg = "200 OK"
+            print(msg)
+            socket_client.sendall(msg.encode())
+            continue
+        elif data == "QUIT\n":
+            msg = "200 OK"
+            socket_client.sendall(msg.encode())
+            print("Client has disconnected.")
+            break
+        elif data == "SHUTDOWN\n":
+            sending_msg = "300 PASSWORD REQUIRED"
+            socket_client.sendall(sending_msg.encode())
+            password_attempt = socket_client.recv(1024).decode()
+            print(f"Received password attempt: {password_attempt.strip()}")
+            if password != password_attempt.strip():
+                print("Invalid password received.")
+                sending_msg = "400 INVALID PASSWORD"
+                socket_client.sendall(sending_msg.encode())
+                continue
+            sending_msg = "200 OK SHUTTING DOWN"
+            socket_client.sendall(sending_msg.encode())
+            socket_server.close()
+            break
+        else:
+            break
+
+    if data == "SHUTDOWN\n":
+        print("Server is shutting down.")  
+        break
