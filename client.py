571a88eef166145527a2f8979b33aff032b217e3
ZaneCarlson <146386094+ZaneCarlson@users.noreply.github.com>
2025-04-06 15:20:01 -0400

    added comments

--- client.py
+++ client.py
@@ -1,30 +1,35 @@
 import socket
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python client.py <server_ip_address>")
+if len(sys.argv) != 2: # Check if the correct number of arguments is provided
+    print("Usage: python client.py <server_ip_address>") #display the correct usage of the program
     sys.exit(1)
 
-serverIP = sys.argv[1]
+serverIP = sys.argv[1] # Get the server IP address from the command line argument
 SERVER_PORT = 6789
 
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_client.connect((serverIP, SERVER_PORT))
+# Create a TCP socket and connect to the server
 
-while True:
+while True: # Loop through the clients commands
 
     request = input("Enter your request (MSGGET, MSGSTORE, QUIT, or SHUTDOWN): \nc: ")
     request += "\n"
+    # Take the user input and append a newline character to it
 
     if request == "MSGSTORE\n":
-            socket_client.sendall(request.encode())
-            recieved_msg = socket_client.recv(1024).decode()
-            print("s:" + recieved_msg)
-            sending_msg = input("c:")
-            socket_client.sendall(sending_msg.encode())
-            recieved_msg = socket_client.recv(1024).decode()
-            print("s:" + recieved_msg)
-            continue
+        socket_client.sendall(request.encode())
+        recieved_msg = socket_client.recv(1024).decode()
+        print("s:" + recieved_msg)
+        sending_msg = input("c:")
+        socket_client.sendall(sending_msg.encode())
+        recieved_msg = socket_client.recv(1024).decode()
+        print("s:" + recieved_msg)
+        continue
+        # If the client requests to MSGSTORE then send the message and wait for a 200 OK. 
+        # Then prompt the user for a message to store and send it to the server.
+        # Wait for a 200 OK message and print it to the console. Then continue the loop.
     
     elif request == "QUIT\n":
         socket_client.sendall(request.encode())
@@ -32,6 +37,8 @@ while True:
         if msg == "200 OK":
             print("s:" + msg)
         break
+        # If the client requests QUIT then send the request to the server and wait for 200 OK then break the loop so the connection will be closed.
+        
 
     elif request == "SHUTDOWN\n":
        socket_client.sendall(request.encode())
@@ -45,20 +52,25 @@ while True:
        elif recieved_msg == "400 INVALID PASSWORD":
             print("s:" + recieved_msg)
             continue
+        # If the client requests SHUTDOWN then send the request to the server and wait for a 300 PASSWORD REQUIRED message.
+        # Then prompt the user for a password and send it to the server.
+        # Wait for a 200 OK SHUTTING DOWN message and break the loop to close the connection.
+        # If the password is invalid then print the message and continue the loop.
 
     elif request == "MSGGET\n":
         socket_client.sendall(request.encode())
         recieved_msg = socket_client.recv(1024).decode()
         print("s:" + recieved_msg)
         continue
-
-    
-
+        # If the client requests MSGGET then send that request. then recieve the message of the day and print it to the console.
+        # Then continue the loop.
 
     else:
         print("Invalid request. Please try again.")
         continue
+        # If the request is invalid then print an error message and continue the loop.
 
 
 socket_client.close()
 print("Connection closed.")
+# make sure the connection is closed and end the program.
--- server.py
+++ server.py
@@ -2,26 +2,30 @@ import socket
 
 HOST = '127.0.0.1'
 SERVER_PORT = 6789
-password = "123!abc"
-message = "An apple a day keeps the doctor away."
+password = "123!abc"                              # Password for shutdown command
+message = "An apple a day keeps the doctor away." # Default Message of the day
 
-socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_server.bind((HOST, SERVER_PORT))
+socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # Create a TCP socket
+socket_server.bind((HOST, SERVER_PORT))                             # Bind the socket to the host and port
 socket_server.listen(1)
 print(f"Server is listening on {HOST}:{SERVER_PORT}")
 
-while True:
-    socket_client, client_address = socket_server.accept()
+while True: #loop while waiting for a connection
+    socket_client, client_address = socket_server.accept() # Accept a connection from a client
     print(f"Connection from {client_address} has been established.")
 
 
-    while True:
-        data = socket_client.recv(1024).decode()
+    while True: #loop through the clients commands
+        data = socket_client.recv(1024).decode() # Receive data from the client
         print(f"Received request: {data.strip()}")
-        if data == "MSGGET\n":
+        
+
+        if data == "MSGGET\n":                   
             msg = "200 OK\n" + message
             socket_client.sendall(msg.encode())
             continue
+            #if the client requests the message of the day then send it to them and continue the loop
+
         elif data == "MSGSTORE\n":
             msg = "200 OK"
             socket_client.sendall(msg.encode())
@@ -32,11 +36,15 @@ while True:
             print(msg)
             socket_client.sendall(msg.encode())
             continue
+            #if the client reqests to store a message then prompt them for a message and store it in the variable message and cofirm that it was stored. continue command loop.
+
         elif data == "QUIT\n":
             msg = "200 OK"
             socket_client.sendall(msg.encode())
             print("Client has disconnected.")
             break
+            #if the client requests to quit then send them a 200 OK message and break out of the command loop.
+
         elif data == "SHUTDOWN\n":
             sending_msg = "300 PASSWORD REQUIRED"
             socket_client.sendall(sending_msg.encode())
@@ -51,9 +59,11 @@ while True:
             socket_client.sendall(sending_msg.encode())
             socket_server.close()
             break
+            #if the client requests to shutdown then send them a "password required" message and wait for them to send the password. if the password is correct then send them a 200 OK SHUTTING DOWN message and close the server and break.
         else:
             break
 
     if data == "SHUTDOWN\n":
         print("Server is shutting down.")  
         break
+        #Shutdown command breaks out of the main loop, and breaks out of the connection loop. 
